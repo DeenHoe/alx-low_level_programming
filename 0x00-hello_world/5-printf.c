eedb9400a4d13adade3a9f0d7939201ce1c009e8
DeenHoe <damohdinho@gmail.com>
2023-07-13 13:18:11 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - It is the first function of a C program
+ *
+ * Return: is always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
