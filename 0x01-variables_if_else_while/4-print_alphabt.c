a9601996c68478906e0752fc30f865a26ba94e6e
DeenHoe <damohdinho@gmail.com>
2023-07-14 16:52:03 -0700

    Letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: should be zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
