506c0f5ca8b308f98981ff22bbf597bfd843ed63
DeenHoe <damohdinho@gmail.com>
2023-07-14 17:22:38 -0700

    Using putchar to pront single digits number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: is 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar('a');
+	putchar('\n');
+	return (0);
+}
