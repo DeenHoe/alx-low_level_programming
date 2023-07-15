7a6636e3a580666f94b95ae136df523e5fc6811c
DeenHoe <damohdinho@gmail.com>
2023-07-14 17:10:24 -0700

    single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: is 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%i", a);
+	printf("\n");
+	return (0);
+}
