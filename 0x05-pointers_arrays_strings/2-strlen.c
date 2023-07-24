f2a129aac0a007917c15602cd20f864f721ef213
DeenHoe <damohdinho@gmail.com>
2023-07-24 15:38:06 -0700

    Swaps two integers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - prints the string length
+ * @s: function parameter
+ * Return: returns a, the value of strlen(str)
+ */
+
+int _strlen(char *s)
+{
+	int str;
+	s = &str;
+
+	int a = strlen(str);
+
+	return (a);
+}
