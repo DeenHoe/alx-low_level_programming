f2c28141aeaad552be5fd1a0092bb0f7f6772909
DeenHoe <damohdinho@gmail.com>
2023-07-25 09:41:19 -0700

    Prints an array of numbers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Function to print an array of integers
+ * @a: function parameter
+ * @n: function parameter
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int b, m;
+	m = n - 1;
+
+	for (b = 0 ; b < n ; b++)
+	{
+		if (b == m)
+			printf("%d", a[b]);
+		else
+			printf("%d, ", a[b]);
+	}
+	printf("\n");
+}
