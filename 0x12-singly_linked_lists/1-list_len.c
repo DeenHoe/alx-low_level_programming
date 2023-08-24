aae7f9ffc3be4376c2ab8e704153bd4a1c1403cb
DeenHoe <damohdinho@gmail.com>
2023-08-24 13:16:40 -0700

    A function the returns the number of elements in a linked nlist

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * list_len - a function that returns the number of elements in na linked list
+ * list_len - returns the number of elements in na linked list
  * @h: pointer to the list_t list
+ * Return: ade
  */
 size_t list_len(const list_t *h)
 {
