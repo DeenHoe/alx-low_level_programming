c947f521d79adb056be640216be84dde2a796aa2
DeenHoe <damohdinho@gmail.com>
2023-08-29 12:25:33 -0700

    Functions that adds a new node at the end of a list and at the begining of a list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - add a new node at the beginning of a list
+ * @head: points to the first node
+ * @n: data to insert in that new node
+ * Return: pointer to the new node or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *fresh;
+
+	fresh= malloc(sizeof(listint_t));
+	if (fresh == NULL)
+		return (NULL);
+	fresh->n = n;
+	fresh->next = *head;
+	*head = fresh;
+
+	return (fresh);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element
+ * @n: data to insert
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *men;
+	listint_t *temp = *head;
+
+	Men = malloc(sizeof(listint_t));
+	if (men == NULL)
+		return (NULL);
+	men->n = n;
+	men->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = men;
+		return (men);
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = men;
+
+	return (new);
+}
